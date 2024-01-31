7d17ebe3f83f3772a1055987ef7c02c650ba4898
mikid <daniarov@post.bgu.ac.il>
2024-01-31 15:54:16 +0200

    4th commit, added iteration

--- assignment_1.py
+++ assignment_1.py
@@ -1,28 +1,30 @@
 from github import Github
 import sys
 
-def print_commit_info(branch):
-    first_commit = branch.commit
-    print(f"Branch Name: {branch.name}")
-    print(f"Commit ID (SHA): {first_commit.sha}")
-    print(f"Commit Author Name: {first_commit.commit.author.name}")
-    print(f"Commit Date: {first_commit.commit.author.date}")
-    print(f"Commit Message: {first_commit.commit.message}")
+def print_commit_info(commit):
+    print(f"Commit ID (SHA): {commit.sha}")
+    print(f"Commit Author Name: {commit.commit.author.name}")
+    print(f"Commit Date: {commit.commit.author.date}")
+    print(f"Commit Message: {commit.commit.message}")
     print("-" * 50)
 
 def main():
     if len(sys.argv) != 4:
         print("not enough parameters,should be <repo-owner> <repo-name> <branch-name>")
         sys.exit(1)
-
+    #intialize required parameters
     repo_owner = sys.argv[1]
     repo_name = sys.argv[2]
     branch_name = sys.argv[3]
 
-    g = Github()
-    repo = g.get_repo(f"{repo_owner}/{repo_name}")
+    git = Github()
+    repo = git.get_repo(f"{repo_owner}/{repo_name}")
     branch = repo.get_branch(branch_name)
-    print_commit_info(branch)
+    allCommits = repo.get_commits(sha=branch.commit.sha)
+    
+    #commits works as a queue, latest is at index 0 
+    for commit in allCommits[:5]:
+        print_commit_info(commit)
 
 if __name__ == "__main__":
     main()
